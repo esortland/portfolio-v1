6b1cce0c34184d843b2352456012ec2ef72eb620
Eric Sortland <esortlan@Erics-MacBook-Pro.local>
2020-08-07 15:53:38 -0500

    updating experience and margins

--- src/components/ExperiencePage/ExperiencePage.js
+++ src/components/ExperiencePage/ExperiencePage.js
@@ -8,24 +8,24 @@ const target =
 		<h4 className='timeline'>Jun 20202 - Aug 2020</h4>
 		<hr/>
 		<div className='job_description'>
-		<p>
-			As a member of the Assortment Planning Experience Team (APEX)
-			I completed stories for both the front and back end utilizing tech
-			such as React, NodeJS, Spring Boot, Java, and MongoDB. 
-		</p>
-		<br/>
-		<p>
-			Propsed a larger project of redesigning the Create Assortment Plan
-			core compnenet and its children. Leveraging Formik I was able to simplify 
-			all form logic into one componenet where previously it was split into three.
-			This resulted in improved code readability, maintainablitiy, and overall 
-			user experience.
-		</p>
-		<br/>
-		<p>
-			Performed front and backend unit testing to maintain 100% code coverage 
-			with Jest, Enzyme, and JUnit
-		</p>
+			<p>
+				As a member of the Assortment Planning Experience Team (APEX)
+				I completed stories for both the front and back end utilizing tech
+				such as React, NodeJS, Spring Boot, Java, and MongoDB. 
+			</p>
+			<br/>
+			<p>
+				Propsed a larger project of redesigning the Create Assortment Plan
+				core compnenet and its children. Leveraging Formik I was able to simplify 
+				all form logic into one componenet where previously it was split into three.
+				This resulted in improved code readability, maintainablitiy, and overall 
+				user experience.
+			</p>
+			<br/>
+			<p>
+				Performed front and backend unit testing to maintain 100% code coverage 
+				with Jest, Enzyme, and JUnit
+			</p>
 		</div>
 	</React.Fragment>
 
@@ -35,7 +35,19 @@ const groupLens =
 		<h4 className='employer'>Group Lens</h4>
 		<h4 className='timeline'>Aug 2019 - May 2020</h4>
 		<hr/>
-		<p>Descrption</p>
+		<div className='job_description'>
+			<p>
+				Worked alongside GroupLens graduate and undergraduate researchers
+				on social computing projects and studies 
+			</p>
+			<br/>
+			<p>
+				Contributed to the development of Flip*Doubt, a web application 
+				built upon React, NodeJS, Express, and MongoDB,
+				tasked with collecting negative user thoughts and reframing them 
+				in a poisitve light using the Mechanical Turk API
+			</p>
+		</div>
 	</React.Fragment>
 
 const uofm = 
@@ -44,7 +56,22 @@ const uofm =
 		<h4 className='employer'>U of M Math Dept.</h4>
 		<h4 className='timeline'>May 2019 - Aug 2020</h4>
 		<hr/>
-		<p>Description</p>
+		<div className='job_description'>
+			<p>
+				Worked under Dr. William Leeb on an exploration of statistical 
+				methods leveraging computational resources and knowledge
+			</p>
+			<br/>
+			<p>
+				Proved the James-Stein estimator is a more accurate mean estimator
+				of a multivariate normal distribution than a Least-Squares approach
+			</p>
+			<br/>
+			<p>
+				Wrote a python script to run Monte Carlo simulations on random synthetic 
+				data to see increased accuracy rates compared to the Least-Squares Method
+			</p>
+		</div>
 	</React.Fragment>
 
 const pages = [
