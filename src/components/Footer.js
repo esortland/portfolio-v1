6d6d6901678160bd83a16373283806f71522ec90
Eric Sortland <esortlan@Erics-MacBook-Pro.local>
2020-08-19 14:24:26 -0500

    adding email to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
 		<footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-			<ul className="info">
+			<ul className="info" style={{paddingLeft: '0rem'}}>
 				<li>
-						Code on <a target="_blank" href="https://www.github.com/esortland/portfolio-v1">GitHub</a>. 
+					Code on <a target="_blank" href="https://www.github.com/esortland/portfolio-v1">GitHub</a>. 
 				</li>
 				<li>
 						Created by <a target="_blank" href="https://www.github.com/esortland">Eric Sortland</a>
 				</li>
 				<li>
-						Built with <a target="_blank" href="https://www.gatsbyjs.com/">Gatsby.js</a>
+					Built with <a target="_blank" href="https://www.gatsbyjs.com/">Gatsby.js</a>
+				</li>
+			</ul>
+			<ul className="info" style={{paddingLeft: '0rem'}}>
+				<li style={{textTransform: 'none'}}>
+					<a href="mailto:esortlan@gmail.com">esortlan@gmail.com</a>
 				</li>
 			</ul>
 		</footer>
